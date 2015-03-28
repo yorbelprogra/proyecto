885c4affb927a94292d0dd2a4c29ff04436c059c
yorbeljavier1408@hotmail.com <yorbel8perez@gmail.com>
2015-03-28 13:59:54 -0430

    un poco de DOM_Tree

--- DOM_Tree.cpp
+++ DOM_Tree.cpp
@@ -5,6 +5,53 @@
  *      Author: yorbel y samuel
  */
 #include "DOM_Tree.h"
+DOM_Tree::DOM_Tree()
+{
+	raiz=NULL;
+}
+DOM_Tree::DOM_Tree(Element r, list<DOM_Tree> hijos)
+{
+	raiz= new Node(r,NULL,NULL);
+	
+	if(!hijos.empty())
+	{
+		Node *aux;
+		raiz->setFirstChild(retornarCopia(hijos.front().raiz));
+		hijos.pop_front();
+		aux=raiz->getFirstChild();
+		while(!hijos.empty())
+		{
+			aux->setNextSibling( retornarCopia(hijos.front().raiz) );
+			hijos.pop_front();
+			aux=aux->getNextSibling();
+		}
+	
+	}
+}
+DOM_Tree::DOM_Tree(const DOM_Tree &fuente)
+{
 
-
-
+	this->raiz=retornarCopia(fuente.raiz);
+}
+Node* DOM_Tree::retornarCopia(Node *p) const
+{
+	 
+	if(p==NULL)
+	{
+		return(NULL);
+	}
+	else
+	{
+		Node *nuevo = new Node;
+		nuevo->setElement( p->getElement());
+		nuevo->setFirstChild(retornarCopia(p->getFirstChild()));
+		nuevo->setNextSibling(retornarCopia(p->getNextSibling()));
+		return(nuevo);
+ 	}
+	
+}
+DOM_Tree DOM_Tree::childNode(int p)
+{
+	DOM_Tree hijo;
+	if()
+}
--- DOM_Tree.h
+++ DOM_Tree.h
@@ -8,12 +8,17 @@
 #ifndef DOM_TREE_H_
 #define DOM_TREE_H_
 #include "Node.h"
-
+#include <list>
 class DOM_Tree
 {
 	private:
-
+		Node *raiz;
+		Node *retornarCopia(Node *p);
 	public:
+		DOM_Tree();
+		DOM_Tree(Element r, list<DOM_Tree> hijos);
+		DOM_Tree(const DOM_Tree &fuente) const;
+		DOM_Tree childNode(int p);
 };
 
 
